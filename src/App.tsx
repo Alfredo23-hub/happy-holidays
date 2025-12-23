3af0e5188b31729c36fa709297b5bd01d1aa56d2
alfredo.bonaldo <a.bonaldo@prismaprogetti.it>
2025-12-23 10:56:36 +0100

    add animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+import { confetti } from "tsparticles-confetti";
+
+const generaColoreEsadecimale = (): string => {
+    const lettere = '0123456789ABCDEF';
+    let colore = '#';
+    for (let i = 0; i < 6; i++) {
+      colore += lettere[Math.floor(Math.random() * 16)];
+    }
+    return colore;
+  }
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const confettiOptions = {
+    angle: 90,
+    count: 500,
+    position: {
+      x: 50,
+      y: 50
+    },
+    spread: 45,
+    startVelocity: 45,
+    decay: 0.9,
+    gravity: 1,
+    drift: 0,
+    ticks: 200,
+    colors: ["#ffffff"],
+    shapes: ["square", "circle", "star", "heart", "spades", "dimonds"],
+    scalar: 1,
+    zIndex: 100,
+    disableForReducedMotion: true
+  };
+
+  const confettiClickOptions = { ...confettiOptions }
+
+  const handleFunction = (event: MouseEvent) => {
+    const xScaled = (event.clientX / window.innerWidth) * 100;
+    const yScaled = (event.clientY / window.innerHeight) * 100;
+    confettiClickOptions.position = { x: xScaled, y: yScaled };
+    confettiClickOptions.colors.push(generaColoreEsadecimale());
+    (async () => {
+      await confetti("tsparticles", confettiClickOptions);
+    })();
+  };
+
+  useEffect(() => {
+    document.body.onclick = (event) => handleFunction(event);
+    setInterval(() => (async () => {
+      confettiOptions.colors.push(generaColoreEsadecimale(), generaColoreEsadecimale());
+      await confetti("tsparticles", confettiOptions);
+    })(), 2000);
+  }, []);
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Happy Holidays Frodo</h1>
+        <h2>Some of magics will happen!</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
